1cf72615b8d952e58a2304980c7d96af3b45b830
Gangu <bxtcheshouse@gmail.com>
2021-11-25 18:39:42 -0500

    dltzk sorter added

---
+++ dltzksort/src/js/data/2021-11-25.js
@@ -0,0 +1,578 @@
+dataSetVersion = "2021-11-25"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Album/Project",
+    key: "album",
+    tooltip: "Check this to restrict to certain albums/projects.",
+    checked: false,
+    sub: [
+      { name: "Singles & Other Stuff", key: "sin" },
+      { name: "Teen Week", key: "tw" },
+      { name: "Frailty", key: "fr" },
+      { name: "No Words, Just a Picture of Me", key: "nw" },
+      { name: "Dariacore S1", key: "dc1" },
+      { name: "Dariacore S2", key: "dc2" },
+      { name: "Dariacore S3", key: "dc3" },
+      { name: "Other Dariacore", key: "otd" },	  
+	  { name: "TwerkNation28", key: "t28" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "let down",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+  {
+    name: "homeswitcher",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "52 blue mondays",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "dysphoria",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "cartridge",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "beast friend",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "woodside gardens 16 december 2012",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "seventeen",
+    img: "A4teLvc.png",
+    opts: {
+      album: [ "tw" ]
+    }
+  },
+    {
+    name: "goldfish",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "your clothes",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "misplace",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "pretender",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "search party",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "buzzcut, daisy",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "movies for guys",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "kodak moment",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "can you tell?",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "how to lie",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "champ",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "eyes off the wheel, i'm a star",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "let's go home",
+    img: "O4KvAoq.png",
+    opts: {
+      album: [ "fr" ]
+    }
+  },
+    {
+    name: "yeah",
+    img: "KmZ3jzD.png",
+    opts: {
+      album: [ "nw" ]
+    }
+  },
+    {
+    name: "no!!!",
+    img: "KmZ3jzD.png",
+    opts: {
+      album: [ "nw" ]
+    }
+  },
+    {
+    name: "total drama",
+    img: "KmZ3jzD.png",
+    opts: {
+      album: [ "nw" ]
+    }
+  },
+    {
+    name: "lindsaycore",
+    img: "KmZ3jzD.png",
+    opts: {
+      album: [ "nw" ]
+    }
+  },
+    {
+    name: "ricky bobby",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "parental rift",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "go white enby go",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "outside",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "1235",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "theyfriend",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "2008",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "copyright strike my fucking nuts",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "will work for food",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "bluuuueeeee",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "die in my dream",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "turmoiled",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "dessert",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "2on",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "shashshashshash",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "virginity rockstar",
+    img: "nuizRKb.png",
+    opts: {
+      album: [ "dc1" ]
+    }
+  },
+    {
+    name: "dilf repellent",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "stupid hoe",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "...during pride month?",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "video game lover vs. hydraulic press",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "dyed my hair black",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "starbucks employee vs. niche twitter personality @c0ncernn",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "wants mom to know she looks cool and doesn't plan on changing",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "snare of a lifetime",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "the dariacore to YTP pipeline",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "i never go swimming without my lip liner",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "awesome ends with ME and ugly starts with U",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "we love your vibe",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "lil mama vip tickets",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "she was a star, now she works in toronto",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "yes i have a girlfriend and her IQ is 4​,​037",
+    img: "qkfxoyr.png",
+    opts: {
+      album: [ "dc2" ]
+    }
+  },
+    {
+    name: "the joke is on you",
+    img: "p1D1tNQ.png",
+    opts: {
+      album: [ "dc3" ]
+    }
+  },
+    {
+    name: "her head is soooo rolling!! love her",
+    img: "jFSplyD.png",
+    opts: {
+      album: [ "dc3" ]
+    }
+  },
+    {
+    name: "it's giving cher",
+    img: "BKf0CPC.png",
+    opts: {
+      album: [ "otd" ]
+    }
+  },
+    {
+    name: "it's a vicious cycle",
+    img: "VglybxR.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "me",
+    img: "awav66J.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "scarecrow",
+    img: "S1phbEB.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "what's my age again ?",
+    img: "LmnZbRD.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "if you make music.. stop it",
+    img: "8UmWndr.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "shooting star",
+    img: "w4gO1Qf.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "nuisance",
+    img: "6wd6GoQ.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "0 - 0",
+    img: "gB0408W.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "hermit",
+    img: "IHtHPPA.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "hatchets",
+    img: "ul6Tqn2.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "the party i never had",
+    img: "sgt1wBJ.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "so pretty i could die",
+    img: "q8m0Hw9.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "u dont know anything about me",
+    img: "Pdk7C7n.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "ache",
+    img: "vych55P.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "call my phone",
+    img: "pyscJD0.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+    {
+    name: "listerine",
+    img: "A5pHWgG.png",
+    opts: {
+      album: [ "sin" ]
+    }
+  },
+     {
+    name: "#TwerkNation Presents WHY AM I SIDEWAYS!?!?! #WHY AM I SIDEWAYS Mix (2006)",
+    img: "2MmMpCj.png",
+    opts: {
+      album: [ "t28" ]
+    }
+  },
+     {
+    name: "DLTZK ★ ADDISON RAE (iPodOnTheBeat) OBSESSED [SWITCHED] @JustLikeYou",
+    img: "DgsYwNr.png",
+    opts: {
+      album: [ "t28" ]
+    }
+  },
+     {
+    name: "djBACKSPACEZACH \"CAUSE AN ACCIDENT FOR Mii MIX 2022\" (Remaster)",
+    img: "svRD0Ir.png",
+    opts: {
+      album: [ "t28" ]
+    }
+  },
+     {
+    name: "BABYARIEL BIG STEAK NO MAKEUP MIX #FREEBRITNEY",
+    img: "f8EmIm3.png",
+    opts: {
+      album: [ "t28" ]
+    }
+  }
+];
